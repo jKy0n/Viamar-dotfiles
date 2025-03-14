6360bf01bfaaed0c21773d5a58e7d39b41b50fd1
jKy0n <jk.1989@hotmail.com>
2025-03-14 10:00:45 -0300

    Primeiro registro de dotfiles

---
+++ private_dot_config/awesome/executable_autorun.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# run() {
+#   if ! pgrep -f "$1" ;
+#   then
+#     "$@"&
+#   fi
+# }
+
+feh --bg-center ~/Pictures/Wallpapers/blueNebula.jpg &
+picom -b
+# picom -b --animations --animation-window-mass 0.5 --animation-for-open-window zoom --animation-stiffness 350
+# picom --experimental-backend &
+# nice -n 19 xscreensaver &
+# sleep 1 && /usr/libexec/polkit-gnome-authentication-agent-1 &
