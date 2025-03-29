d1364da9e4a68ba747194ad70ae6c3f3220a0ffa
jKy0n <jk.1989@hotmail.com>
2025-03-29 11:18:07 -0300

    Add PATH="$HOME/.local/bin:$PATH on zsh.env"

--- .config/zsh/conf.d/env.zsh
+++ .config/zsh/conf.d/env.zsh
@@ -2,6 +2,8 @@
 export EDITOR=nvim
 export SUDO_EDITOR=nvim
 
+export PATH="$HOME/.local/bin:$PATH"
+
 export PAY_RESPECTS_REQUIRE_CONFIRMATION='true'  # pay-respect: Pergunta antes de aplicar
 
 # Garante ZSH como shell padrão no TMUX
