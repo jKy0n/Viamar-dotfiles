5f60eb4d82b5f1eaa684be88f6e4fec72648e7c7
jKy0n <jk.1989@hotmail.com>
2025-07-04 12:54:17 -0300

    zsh: setting up ccache to run cache in ~/.build (NVMe part)

--- .config/zsh/conf.d/env.zsh
+++ .config/zsh/conf.d/env.zsh
@@ -2,6 +2,16 @@
 export EDITOR=nvim
 export SUDO_EDITOR=nvim
 
+# Define compilações em ~/.build
+export CARGO_HOME="$HOME/.build/cargo"
+export CARGO_TARGET_DIR="/home/jkyon/.build/cargo-target"
+export PIP_CACHE_DIR="/home/jkyon/.build/pip-cache"
+
+# Configurações do ccache
+export CCACHE_DIR="$HOME/.build/ccache"
+export CCACHE_COMPRESS=1
+export CCACHE_MAXSIZE=10G
+
 export PATH="$HOME/.local/bin:$PATH"
 
 export PAY_RESPECTS_REQUIRE_CONFIRMATION='true'  # pay-respect: Pergunta antes de aplicar
