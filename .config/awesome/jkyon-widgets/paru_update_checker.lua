bb984529a8bd2e432234c111d533ac4974c90c9c
jKy0n <jk.1989@hotmail.com>
2025-07-05 10:34:27 -0300

    awsm: jkyon-widgets/paru_update_checker.lua working PERFECTyadm status & picom: disable opacity in tooltips box

--- .config/awesome/jkyon-widgets/paru_update_checker.lua
+++ .config/awesome/jkyon-widgets/paru_update_checker.lua
@@ -2,6 +2,10 @@ local awful = require("awful")
 local wibox = require("wibox")
 local beautiful = require("beautiful")
 
+local function rtrim(s)
+    return s:match("^(.-)%s*$")
+end
+
 local pkg_widget = wibox.widget.textbox()
 
 local pkg_tooltip = awful.tooltip({
@@ -11,9 +15,7 @@ local pkg_tooltip = awful.tooltip({
     bg = beautiful.bg_normal,
     fg = beautiful.fg_normal,
     font = beautiful.font,
-    -- border_width = beautiful.border_width or 2,
-    -- border_color = beautiful.border_focus or "#8aadf4",
-    -- opacity = 1,
+    opacity = 1,
 })
 
 local function update_widget(widget)
@@ -23,14 +25,19 @@ local function update_widget(widget)
             count = count + 1
         end
         widget.markup = count > 0 and ("<span font='MesloLGS Nerd Font Bold'>  " .. count .. " Pkgs |</span>") or ""
-        pkg_tooltip.text = (stdout ~= "" and stdout or "Nenhuma atualização disponível")
+        pkg_tooltip.text = (stdout ~= "" and rtrim(stdout) or "Nenhuma atualização disponível")
     end)
 end
 
-awful.widget.watch("sleep 3600", 3600, update_widget, pkg_widget)   --  Update every 5 minutes
+-- Atualiza imediatamente ao iniciar
+update_widget(pkg_widget)
+
+-- E depois usa o timer
+awful.widget.watch("sleep 3600", 3600, update_widget, pkg_widget)   -- 1 hora
 
 pkg_widget:connect_signal("button::press", function(_, x, y, button)
     if button == 1 then
+        -- Esquerdo: tooltip on/off
         if not pkg_tooltip.visible then
             pkg_tooltip.visible = true
             pkg_tooltip.x = mouse.current_widget_geometry.x
@@ -38,6 +45,9 @@ pkg_widget:connect_signal("button::press", function(_, x, y, button)
         else
             pkg_tooltip.visible = false
         end
+    elseif button == 3 then
+        -- Direito: atualizar agora
+        update_widget(pkg_widget)
     end
 end)
 
