dfa525369b6446b6110276da444002e19042f2f8
jKy0n <jk.1989@hotmail.com>
2025-12-12 10:39:19 -0300

    awsm: wibar add comments

--- .config/awesome/modules/wibar.lua
+++ .config/awesome/modules/wibar.lua
@@ -40,20 +40,6 @@ tbox_separator_space = wibox.widget.textbox (" ")
 tbox_separator_pipe = wibox.widget.textbox (" | ")
 tbox_separator_dash = wibox.widget.textbox (" - ")
 
--- local function styled_textbox(text, font_size, margins)
---     return wibox.widget {
---         text = text,
---         font = 'MesloLGS Nerd Font ' .. font_size,
---         widget = wibox.widget.textbox,
---         margins = margins
---     }
--- end
---
---- Icons for widgets ---
--- local cpu_icon = styled_textbox('  ', 11, 2)
--- local ram_icon = styled_textbox('   ', 11, 2)
--- local gpu_icon = styled_textbox(' 󰢮 ', 16, 1)
--- local temp_icon = styled_textbox(' ', 11, 1)
 
 -- Fixing CPU width on wibox
 local cpu_usage = awful.widget.watch('bash -c "sh /home/jkyon/ShellScript/Viamar-PC/StatusBar-Scripts/CPU-usage-monitor.sh"', 1)
@@ -106,9 +92,9 @@ function wibar.setup(s)
 
             internet_widget,        --  Internet widget detect internet connection
             tbox_separator_space,
-            pkg_widget, --  Paru update checker widget    <<<<<<
+            pkg_widget,             --  Paru update checker widget
             tbox_separator_space,
-            cpu_monitor({"usage", "freq", "temp"}),           --  Graphical CPU usage widget
+            cpu_monitor({"usage", "freq", "temp"}),     --  Graphical CPU usage widget
             tbox_separator_pipe,    --  |
             cpu_widget(),           -- Graphical CPU usage widget
             tbox_separator_pipe,    --  |
@@ -122,7 +108,7 @@ function wibar.setup(s)
             volume_widget({         -- Volume control widget
                 widget_type = 'arc',
                 thickness   = 2 ,
-                step        = 5 ,
+                step        = 5 ,   -- 5% volume change per scroll
                 mixer_cmd   = 'pavucontrol',
                 device      = '@DEFAULT_SINK@',
                 tooltip     = false
@@ -134,11 +120,11 @@ function wibar.setup(s)
             dnd_widget,             -- Do Not Disturb widget
             weather_api_widget({
                 api_key = 'b08df374f2a4412d887190759250711',
-                coordinates = {'-24.0058', '-46.4028'},
+                coordinates = {'-24.0058', '-46.4028'}, -- Praia Grande, SP, Brazil
                 units = 'metric',
                 show_daily_forecast = true,
                 show_hourly_forecast = true,
-                timeout = 1800,
+                timeout = 1800,     -- 30 minutes
                 lang = 'en',
             }),
             tbox_separator_dash,
@@ -151,6 +137,7 @@ function wibar.setup(s)
                 onsuspend  =  function() awful.spawn.with_shell("systemctl suspend") end,
                 onreboot   =  function() awful.spawn.with_shell("systemctl reboot") end,
                 onpoweroff =  function() awful.spawn.with_shell("systemctl poweroff") end,
+
             },
         },
     }
@@ -186,13 +173,6 @@ function wibar.setup(s)
 ------------------------------------------------------------------------------------------------
 
             dnd_widget,     -- Do Not Disturb widget
-            -- weather_api_widget({
-            --     api_key='b08df374f2a4412d887190759250711',
-            --     coordinates = {-24.0058, -46.4028},
-            --     show_daily_forecast = true,
-            --     show_hourly_forecast = true,
-            -- }),
-            -- tbox_separator_dash,
             mytextclock,    -- Text clock widget
 
             logout_menu_widget{ -- Logout menu widget
