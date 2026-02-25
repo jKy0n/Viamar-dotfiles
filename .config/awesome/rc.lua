1e250862f82b5d24ae743e5b120f0a9dec784418
jKy0n <jk.1989@hotmail.com>
2026-02-25 13:30:49 -0300

    awsm: remove lgi temporary fix

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -12,19 +12,6 @@
 --]]
 
 --------------------------------------------------------------
----------------  Temporary workaround for lgi  ---------------
-
--- local lgi = require("lgi")
--- local Gio = lgi.Gio
--- local GioUnix = lgi.GioUnix
-
--- -- compatibilidade: se Gio.UnixInputStream não existir, aponta para GioUnix.InputStream
--- if not Gio.UnixInputStream and GioUnix then
---     Gio.UnixInputStream = GioUnix.InputStream
---     Gio.UnixOutputStream = GioUnix.OutputStream
--- end
-
---------------------------------------------------------------
 -----------------------  First steps  ------------------------
 -- Load luarocks
 pcall(require, "luarocks.loader")
