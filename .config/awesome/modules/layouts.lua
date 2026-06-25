b8ebfce1c07958eb7c9667217550505316cd28dc
jKy0n <jk.1989@hotmail.com>
2026-06-25 12:09:47 -0300

    awsm layout module: just trimming unused layout, lines and comments

--- .config/awesome/modules/layouts.lua
+++ .config/awesome/modules/layouts.lua
@@ -1,15 +1,18 @@
 --[[
-        jKyon (John Kennedy Loria Segundo)
-        layouts.lua – awesomeWM
-        2025-07-10
-
-        Purpose:
-            Custom layout configurations for AwesomeWM.
-            Defines a set of layouts to be used in the window manager, enhancing user experience and workflow.
-
-            Configurações de layout personalizadas para o AwesomeWM.
-            Define um conjunto de layouts a serem usados no gerenciador de janelas, melhorando a experiência do usuário e o fluxo de trabalho.
+--       Title:      layouts.lua
+--       Brief:
+--       Path:       /home/jkyon/.config/awesome/modules/layouts.lua
+--       Author:     John Kennedy a.k.a. jKyon
+--       Created:    2025-07-10
+--       Updated:    2026-06-25
+--       Notes:
+--                   Custom layout configurations for AwesomeWM.
+--                   Defines a set of layouts to be used in the window manager, enhancing user experience and workflow.
+--
+--                   Configurações de layout personalizadas para o AwesomeWM.
+--                   Define um conjunto de layouts a serem usados no gerenciador de janelas, melhorando a experiência do usuário e o fluxo de trabalho.
 --]]
+
 
 --------------------------------------------------------------
 ----------------------  Load Libraries  ----------------------
@@ -32,9 +35,6 @@ awful.layout.layouts = {
 
     -- maximized layout
     awful.layout.suit.max,
-
-    -- floating layout
-    -- awful.layout.suit.floating,
 }
 
 return layouts
